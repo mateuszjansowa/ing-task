5ff3944748a6318d859664330c6974fcc27750d4
mateuszjansowa <mateusz.jan.sowa@gmail.com>
2023-03-14 10:37:33 +0100

    render next and prev buttons

--- src/components/MultiStepForm.js
+++ src/components/MultiStepForm.js
@@ -1,14 +1,17 @@
 import {html, LitElement} from '@lion/core'
 import {loadDefaultFeedbackMessages} from '@lion/ui/validate-messages.js'
 import {API} from '../API'
+import '@lion/ui/define/lion-button.js'
+import '@lion/ui/define/lion-button-submit.js'
 import '@lion/ui/define/lion-form.js'
 import '@lion/ui/define/lion-input.js'
+import '@lion/ui/define/lion-steps.js'
 
 const api = new API()
 
 export class MultiStepForm extends LitElement {
     static properties = {
-        step: {type: Number, state: true},
+        step: {type: Number},
         fields: {type: Array, state: true},
         formState: {type: Object, state: true},
     }
@@ -17,9 +20,7 @@ export class MultiStepForm extends LitElement {
         super()
         this.step = 0
         this.fields = []
-        this.formState = {
-            
-        }
+        this.formState = {}
     }
 
     connectedCallback() {
@@ -29,11 +30,24 @@ export class MultiStepForm extends LitElement {
         })
     }
 
-    step1 = html` <lion-button @click=${() => this.step++}>Next</lion-button> `
+    renderButton = () =>
+        ({
+            0: html`<lion-button @click=${() => this.step++}>
+                Next
+            </lion-button>`,
+            1: html`<lion-button @click=${() => this.step--}>
+                    Previous
+                </lion-button>
+                <lion-button-submit @click=${() => this.step++}>
+                    Submit
+                </lion-button-submit>`,
+        }[this.step])
 
-    step2 = html` <lion-button @click=${() => this.step--}>
-        Previous
-    </lion-button>`
+    renderFields = () =>
+        this.fields.map(
+            field =>
+                html`<form-step .field=${field} .step=${this.step}></form-step>`
+        )
 
     render() {
         loadDefaultFeedbackMessages()
@@ -41,16 +55,8 @@ export class MultiStepForm extends LitElement {
         return html`
             <lion-form>
                 <form>
-                    ${this.fields.map(
-                        step =>
-                            html`<form-step
-                                order=${step.order - 1}
-                                name=${step.name}
-                                .elements=${step.form}
-                                .step=${this.step}
-                                .state=${this.formState[step.name]}
-                            ></form-step>`
-                    )}
+                    ${this.renderFields()}
+                    <div>${this.renderButton()}</div>
                 </form>
             </lion-form>
         `
--- src/components/Step.js
+++ src/components/Step.js
@@ -4,22 +4,15 @@ import '@lion/ui/define/lion-button-submit.js'
 
 export default class FormStep extends LitElement {
     static properties = {
-        name: {type: String, attribute: 'name'},
-        elements: {type: Object},
+        field: {type: Object},
         step: {type: Number},
-        order: {type: Number, attribute: 'order'},
-        state: {type: Object},
-    }
-
-    firstUpdated() {
-        console.log(this.elements)
-        console.log(this.state)
     }
 
     render() {
-        if (this.step === this.order) {
-            return html`<h1>${this.name}</h1> `
-        }
+        const {name} = this.field
+        return html`<div ?hidden=${this.field.order - 1 !== this.step}>
+            <h1>${name}</h1>
+        </div>`
     }
 }
 
