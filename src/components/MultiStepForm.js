1200dfa3ee5abbbd1970580648f34d57589cfad8
mateuszjansowa <mateusz.jan.sowa@gmail.com>
2023-03-15 12:44:53 +0100

    get state from step, correct step validation

--- src/components/MultiStepForm.js
+++ src/components/MultiStepForm.js
@@ -3,8 +3,6 @@ import {API} from '../API'
 import '@lion/ui/define/lion-button.js'
 import '@lion/ui/define/lion-button-submit.js'
 
-const api = new API()
-
 export class MultiStepForm extends LitElement {
     static properties = {
         step: {type: Number},
@@ -15,24 +13,30 @@ export class MultiStepForm extends LitElement {
         super()
         this.step = 0
         this.fieldsFromJSON = []
+        this.api = new API()
     }
 
     connectedCallback() {
         super.connectedCallback()
-        api.getData().then(data => {
+        this.api.getData().then(data => {
             this.fieldsFromJSON = data
         })
     }
 
-    renderButton = () =>
-        ({
+    renderButton = () => {
+        const buttons = {
             0: html`<lion-button @click=${() => this.step++}> Next </lion-button>`,
             1: html`<lion-button @click=${() => this.step--}> Previous </lion-button>
                 <lion-button-submit @click=${this.submitViaJS}> Submit </lion-button-submit>`,
             2: html`<h1>Thanks for filling out the form!</h1>`,
-        }[this.step])
+        }
 
-    renderSteps = () =>
+        if (Object.hasOwn(buttons, this.step)) {
+            return buttons[this.step]
+        }
+    }
+
+    renderFormSteps = () =>
         this.fieldsFromJSON.map(
             field => html` <form-step .field=${field} .step=${this.step}></form-step>`
         )
@@ -40,7 +44,7 @@ export class MultiStepForm extends LitElement {
     render() {
         return html`
             <form @submit=${this.onSubmit}>
-                ${this.renderSteps()}
+                ${this.renderFormSteps()}
                 <div>${this.renderButton()}</div>
             </form>
         `
@@ -48,26 +52,16 @@ export class MultiStepForm extends LitElement {
 
     onSubmit = e => {
         e.preventDefault()
-        const formData = new FormData()
         const formSteps = this.shadowRoot.querySelectorAll('form-step')
+
         if (formSteps && formSteps.length > 0) {
             for (const formStep of formSteps) {
-                const lionFormStep = formStep.shadowRoot.querySelector('lion-form')
-                // this is how you detect error in a given step
-                // TODO error detecion
-                if (lionFormStep.hasFeedbackFor.includes('error')) {
+                // todo error detection
+                if (formStep.lionForm.hasFeedbackFor.includes('error')) {
                     console.log('error')
-                }
-                const stepFormData = new FormData(formStep.shadowRoot.querySelector('form'))
-                for (const [key, value] of stepFormData.entries()) {
-                    formData.append(key, value)
                 }
             }
         }
-
-        /*  for (const [key, value] of formData.entries()) {
-            console.log(key, value)
-        } */
     }
 }
 
