410bc370dd90829d6e5043d04f16d68ae6477ab6
mateuszjansowa <mateusz.jan.sowa@gmail.com>
2023-03-15 09:32:29 +0100

    create basic submit function

--- src/components/FormStep.js
+++ src/components/FormStep.js
@@ -1,5 +1,5 @@
 import {LitElement, html} from '@lion/core'
-import {formConverter} from '../helpers'
+import {formConverter, isHidden} from '../helpers'
 import {loadDefaultFeedbackMessages} from '@lion/ui/validate-messages.js'
 import '@lion/ui/define/lion-button.js'
 import '@lion/ui/define/lion-button-submit.js'
@@ -9,7 +9,7 @@ import '@lion/ui/define/lion-option.js'
 
 export default class FormStep extends LitElement {
     static properties = {
-        form: {type: Array, state: true},
+        form: {type: Array},
         field: {type: Object},
         step: {type: Number},
         state: {type: Object},
@@ -22,37 +22,38 @@ export default class FormStep extends LitElement {
 
     render() {
         const {name} = this.field
-        console.log(this.form)
+        console.log(this.field.order - 1 === this.step && this.form)
         loadDefaultFeedbackMessages()
 
         return html`<div ?hidden=${this.field.order - 1 !== this.step}>
             <h1>${name}</h1>
-            ${this.form.map(field => {
-                if (['text', 'number'].includes(field.type)) {
-                    return html`<lion-input
-                        .type=${field.type}
-                        .name=${field.label}
-                        .label=${field.label}
-                        .validators="${field.validators}"
-                        .value=${this.state[field.label]}
-                    ></lion-input>`
-                }
-                if (field.type === 'select') {
-                    return html`<lion-select
-                        .name=${field.label}
-                        .label=${field.label}
-                        .validators="${field.validators}"
-                        .value=${this.state[field.label]}
-                    >
-                        <select slot="input">
-                            <option value select hidden>Select an option</option>
-                            ${field.dataset.map(option => {
-                                return html`<option .value=${option}>${option}</option>`
-                            })}
-                        </select>
-                    </lion-select>`
-                }
-            })}
+            <form>
+                ${this.form.map(field => {
+                    if (['text', 'number'].includes(field.type)) {
+                        return html`<lion-input
+                            type=${field.type}
+                            name=${field.label}
+                            label=${field.label}
+                            .validators="${field.validators}"
+                            ?hidden=${isHidden(field, this.state)}
+                        ></lion-input>`
+                    }
+                    if (field.type === 'select') {
+                        return html`<lion-select
+                            name=${field.label}
+                            label=${field.label}
+                            .validators="${field.validators}"
+                        >
+                            <select slot="input">
+                                <option value select hidden>Select an option</option>
+                                ${field.dataset.map(option => {
+                                    return html`<option .value=${option}>${option}</option>`
+                                })}
+                            </select>
+                        </lion-select>`
+                    }
+                })}
+            </form>
         </div>`
     }
 }
--- src/components/MultiStepForm.js
+++ src/components/MultiStepForm.js
@@ -12,14 +12,12 @@ export class MultiStepForm extends LitElement {
     static properties = {
         step: {type: Number},
         fieldsFromJSON: {type: Array, state: true},
-        formState: {type: Object, state: true},
     }
 
     constructor() {
         super()
         this.step = 0
         this.fieldsFromJSON = []
-        this.formState = {}
     }
 
     connectedCallback() {
@@ -31,37 +29,44 @@ export class MultiStepForm extends LitElement {
 
     renderButton = () =>
         ({
-            0: html`<lion-button @click=${() => this.step++}>
-                Next
-            </lion-button>`,
-            1: html`<lion-button @click=${() => this.step--}>
-                    Previous
-                </lion-button>
-                <lion-button-submit @click=${() => this.step++}>
-                    Submit
-                </lion-button-submit>`,
+            0: html`<lion-button @click=${() => this.step++}> Next </lion-button>`,
+            1: html`<lion-button @click=${() => this.step--}> Previous </lion-button>
+                <lion-button-submit @click=${this.submitViaJS}> Submit </lion-button-submit>`,
             2: html`<h1>Thanks for filling out the form!</h1>`,
         }[this.step])
 
     renderSteps = () =>
         this.fieldsFromJSON.map(
-            field =>
-                html`<form-step
-                    .field=${field}
-                    .step=${this.step}
-                    .state=${this.formState}
-                ></form-step>`
+            field => html` <form-step .field=${field} .step=${this.step}></form-step>`
         )
 
     render() {
         return html`
-            <lion-form>
+            <lion-form @submit=${this.onSubmit}>
                 <form>
                     ${this.renderSteps()}
                     <div>${this.renderButton()}</div>
                 </form>
             </lion-form>
         `
+    }
+
+    onSubmit = e => {
+        e.preventDefault()
+        const formData = new FormData()
+        const formSteps = this.shadowRoot.querySelectorAll('form-step')
+        if (formSteps && formSteps.length > 0) {
+            for (const formStep of formSteps) {
+                const stepFormData = new FormData(formStep.shadowRoot.querySelector('form'))
+                for (const [key, value] of stepFormData.entries()) {
+                    formData.append(key, value)
+                }
+            }
+        }
+
+        for (const [key, value] of formData.entries()) {
+            console.log(key, value)
+        }
     }
 }
 
