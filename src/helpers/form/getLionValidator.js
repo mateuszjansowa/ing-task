39336a0b49e199ac4e9db3660328117ec41875cd
mateuszjansowa <mateusz.jan.sowa@gmail.com>
2023-03-15 12:45:06 +0100

    correct validator assignment method

--- src/helpers/form/getLionValidator.js
+++ src/helpers/form/getLionValidator.js
@@ -7,12 +7,17 @@ import {
     MaxNumber,
 } from '@lion/ui/form-core.js'
 
-export const getLionValidator = (rule = '', options = 0) =>
-    ({
+export const getLionValidator = (rule = '', options = 0) => {
+    const validators = {
         required: new Required(),
         'min-len': new MinLength(+options),
         'max-len': new MaxLength(+options),
         len: new EqualsLength(+options),
         'min-number': new MinNumber(+options),
         'max-number': new MaxNumber(+options),
-    }[rule])
+    }
+
+    if (rule in validators) {
+        return validators[rule]
+    }
+}
