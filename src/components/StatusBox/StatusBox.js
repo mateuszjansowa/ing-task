2e8da4ffe187a54b34cd281946e84a2972671f23
mateuszjansowa <mateusz.jan.sowa@gmail.com>
2023-03-27 13:03:42 +0200

    Refactor StatusBox component to use lit/directives/when for conditional rendering of status icon

--- src/components/StatusBox/StatusBox.js
+++ src/components/StatusBox/StatusBox.js
@@ -1,30 +1,35 @@
 import {LitElement, html} from 'lit'
 import {toTitleCase} from '../../helpers/utils'
 import {serverState} from '../../constants'
+import {when} from 'lit/directives/when.js'
 import styles from './StatusBox.styles'
 import 'material-icon-component/md-icon-outlined.js'
 
 export class StatusBox extends LitElement {
     static styles = styles
     static properties = {
-        formSendStatus: {type: Object},
+        formSendStatus: {type: Object, attribute: false},
     }
 
-    #getIcon = status => {
-        if (status) {
-            return status === serverState.success.status
-                ? html`<md-icon>check-circle</md-icon>`
-                : html`<md-icon>cancel</md-icon>`
+    #getIcon = () => {
+        if (!this.formSendStatus.status) {
+            return
         }
+
+        return when(
+            this.formSendStatus.status === serverState.success.status,
+            () => html`<md-icon>check_circle</md-icon>`,
+            () => html`<md-icon>cancel</md-icon>`
+        )
     }
 
     render() {
-        const {status, message} = this.formSendStatus
+        const {message, status} = this.formSendStatus
         return html`
             <div class="status-box status-box--${status} ">
                 <h3>
                     <span class="status-box__heading"
-                        >${this.#getIcon(status)} ${toTitleCase(status)}
+                        >${this.#getIcon()} ${toTitleCase(status)}
                     </span>
                 </h3>
                 <p>${message}</p>
